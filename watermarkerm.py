74b5fd6f852d60ddb24f02369cbf1eea357050a4
Maxiee <maxieewong@gmail.com>
2014-02-16 00:23:04 +0800

    初步实现功能，细节需要完善

--- watermarkerm.py
+++ watermarkerm.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
+import sys,os
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from imageeditor import ImageEditor, Location
 
 class MainWindow(QWidget):
+    imageEditor = ImageEditor()
+
     def __init__(self, parent = None):
         super(MainWindow, self).__init__(parent)
         layoutImage = QHBoxLayout()
@@ -13,14 +16,14 @@ class MainWindow(QWidget):
         layoutMain = QVBoxLayout()
         #layoutImage布局
         btnImageOpen = QPushButton("打开图片",self)
-        labelImageName = QLabel()
+        self.labelImageName = QLabel()
         layoutImage.addWidget(btnImageOpen)
-        layoutImage.addWidget(labelImageName)
+        layoutImage.addWidget(self.labelImageName)
         #layoutWater布局
         btnWaterOpen = QPushButton("打开水印",self)
-        labelWaterName = QLabel()
+        self.labelWaterName = QLabel()
         layoutWater.addWidget(btnWaterOpen)
-        layoutWater.addWidget(labelWaterName)
+        layoutWater.addWidget(self.labelWaterName)
         #layoutLocGen布局
         labelLocation = QLabel("水印位置:")
         comboLocation = QComboBox()
@@ -34,6 +37,28 @@ class MainWindow(QWidget):
         layoutMain.addLayout(layoutWater)
         layoutMain.addLayout(layoutLocGen)
         self.setLayout(layoutMain)
+        #链接信号槽
+        self.connect(btnImageOpen, SIGNAL("clicked()"), self.onImageOpenClicked)
+        self.connect(btnWaterOpen, SIGNAL("clicked()"), self.onWaterOpenClicked)
+        self.connect(btnGenerate, SIGNAL("clicked()"), self.onGenerateClicked)
+
+    #定义槽函数
+    @pyqtSlot()
+    def onImageOpenClicked(self):
+        path = QFileDialog.getOpenFileName(self, "打开图片", "~/")
+        self.labelImageName.setText(os.path.split(path)[1])
+        self.imageEditor.openImage(path)
+
+    @pyqtSlot()
+    def onWaterOpenClicked(self):
+        path = QFileDialog.getOpenFileName(self, "打开水印", "~/")
+        self.labelWaterName.setText(os.path.split(path)[1])
+        self.imageEditor.openMark(path)
+
+    @pyqtSlot()
+    def onGenerateClicked(self):
+        self.imageEditor.makeMark(Location.RIGHTBUTTON)
+        print("成功！")
 
 if __name__ == "__main__":
     App = QApplication(sys.argv)
